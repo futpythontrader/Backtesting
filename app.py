4dcde907841d5a89e2cc30587061e017a02b5d29
futpythontrader <futpythontrader@gmail.com>
2023-04-07 22:58:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ st.title("Software de Backtesting")
 
 @st.cache
 def load_base():
-    url = "https://github.com/futpythontrader/YouTube/blob/main/x_FutPythonTrader_Base_de_Dados_Temporada_Anterior_x.xlsx?raw=true"
-    data_jogos = pd.read_excel(url)
+    url = "https://github.com/futpythontrader/YouTube/blob/main/Base_de_Dados/futpythontraderpunter.csv?raw=true"
+    data_jogos = pd.read_csv(url)
 
     return data_jogos
 
@@ -18,31 +18,31 @@ df.loc[(df['FT_Goals_H'] >  df['FT_Goals_A']), 'Profit'] = df['FT_Odds_H'] - 1
 df.loc[(df['FT_Goals_H'] <= df['FT_Goals_A']), 'Profit'] = -1
 
 # Caixa de texto para filtrar Odd Home
-min_H = st.sidebar.number_input("FT_Odds_H", min_value=0, max_value=10, value=0)
-max_H = st.sidebar.number_input("FT_Odds_H", min_value=0, max_value=10, value=10)
+min_H = st.sidebar.number_input("FT_Odd_H", min_value=1.01, max_value=10.0, value=1.01)
+max_H = st.sidebar.number_input("FT_Odd_H", min_value=1.01, max_value=10.0, value=10.0)
 
 # Caixa de texto para filtrar Odd Draw
-min_D = st.sidebar.number_input("FT_Odds_D", min_value=0, max_value=10, value=0)
-max_D = st.sidebar.number_input("FT_Odds_D", min_value=0, max_value=10, value=10)
+min_D = st.sidebar.number_input("FT_Odd_D", min_value=1.01, max_value=10.0, value=1.01)
+max_D = st.sidebar.number_input("FT_Odd_D", min_value=1.01, max_value=10.0, value=10.0)
 
 # Caixa de texto para filtrar Odd Away
-min_A = st.sidebar.number_input("FT_Odds_A", min_value=0, max_value=10, value=0)
-max_A = st.sidebar.number_input("FT_Odds_A", min_value=0, max_value=10, value=10)
+min_A = st.sidebar.number_input("FT_Odd_A", min_value=1.01, max_value=10.0, value=1.01)
+max_A = st.sidebar.number_input("FT_Odd_A", min_value=1.01, max_value=10.0, value=10.0)
 
 # Caixa de texto para filtrar Odd Over 25
-min_Over25 = st.sidebar.number_input("FT_Odds_Over25", min_value=0, max_value=10, value=0)
-max_Over25 = st.sidebar.number_input("FT_Odds_Over25", min_value=0, max_value=10, value=10)
+min_Over25 = st.sidebar.number_input("FT_Odd_Over25", min_value=1.01, max_value=10.0, value=1.01)
+max_Over25 = st.sidebar.number_input("FT_Odd_Over25", min_value=1.01, max_value=10.0, value=10.0)
 
 # Caixa de texto para filtrar Odd BTTS
-min_BTTS = st.sidebar.number_input("Odds_BTTS_Yes", min_value=0, max_value=10, value=0)
-max_BTTS = st.sidebar.number_input("Odds_BTTS_Yes", min_value=0, max_value=10, value=10)
+min_BTTS = st.sidebar.number_input("FT_Odd_BTTS_Yes", min_value=1.01, max_value=10.0, value=1.01)
+max_BTTS = st.sidebar.number_input("FT_Odd_BTTS_Yes", min_value=1.01, max_value=10.0, value=10.0)
 
 # Filtre o dataframe pelos valores mínimos e máximos de cada coluna
-df_filtrado = df[(df['FT_Odds_H'] >= min_H) & (df['FT_Odds_H'] <= max_H) &
-                 (df['FT_Odds_D'] >= min_D) & (df['FT_Odds_D'] <= max_D) &
-                 (df['FT_Odds_A'] >= min_A) & (df['FT_Odds_A'] <= max_A) &
-                 (df['FT_Odds_Over25'] >= min_Over25) & (df['FT_Odds_Over25'] <= max_Over25) &
-                 (df['Odds_BTTS_Yes'] >= min_BTTS) & (df['Odds_BTTS_Yes'] <= max_BTTS)]
+df_filtrado = df[(df['FT_Odd_H'] >= min_H) & (df['FT_Odd_H'] <= max_H) &
+                 (df['FT_Odd_D'] >= min_D) & (df['FT_Odd_D'] <= max_D) &
+                 (df['FT_Odd_A'] >= min_A) & (df['FT_Odd_A'] <= max_A) &
+                 (df['FT_Odd_Over25'] >= min_Over25) & (df['FT_Odd_Over25'] <= max_Over25) &
+                 (df['FT_Odd_BTTS_Yes'] >= min_BTTS) & (df['FT_Odd_BTTS_Yes'] <= max_BTTS)]
 
 # Crie uma nova coluna no dataframe filtrado com o profit acumulado
 df_filtrado['Profit_acu'] = df_filtrado.Profit.cumsum()
